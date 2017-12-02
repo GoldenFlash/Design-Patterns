ae0a7a1fc83af9e61cb076612ad57d37f16f4706
wangwei <892458829@qq.com>
2017-12-02 20:30:24 +0800

    add

--- 单列模式.js
+++ 单列模式.js
@@ -18,3 +18,21 @@ var people = (function(){
 var object1= people.createPeople()
 var object2= people.createPeople()
 //object1=object2
+var people = function(){
+	var instance ;
+	var createInstance = function(){
+		this.a=1;
+		this.b=2;
+	}
+	return {
+		init:function(){
+			if(!instance){
+				instance = new createInstance()
+			}
+			return instance;
+		}
+	}
+}
+var a = people();
+var b1 = a.init();
+var b2 = a.init();
